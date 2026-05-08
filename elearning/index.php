0f336ac90e4e9832f9f672d9bd7da307714c9ed0
srrasel <srrasel22a@gmail.com>
2026-05-09 02:42:58 +0600

    Fix Laravel front controller autoload order

--- elearning/index.php
+++ elearning/index.php
@@ -4,6 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/core/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/core/vendor/autoload.php';
+
 $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? getenv('TRUSTED_PROXIES') ?: null;
 if ($trustedProxies) {
     if ($trustedProxies === '*') {
@@ -15,14 +23,6 @@ if ($trustedProxies) {
         }
     }
 }
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/core/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/core/bootstrap/app.php')
