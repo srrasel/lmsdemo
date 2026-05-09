828b3d75ae643ece7efa68d7ff454c82b1315331
srrasel <srrasel22a@gmail.com>
2026-05-09 13:53:09 +0600

    Fix trusted proxy header constants for Symfony Request

--- elearning/index.php
+++ elearning/index.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request as IlluminateRequest;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 define('LARAVEL_START', microtime(true));
 
@@ -14,16 +15,23 @@ require __DIR__.'/core/vendor/autoload.php';
 
 $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? getenv('TRUSTED_PROXIES') ?: null;
 if ($trustedProxies) {
+    $headers = defined(SymfonyRequest::class . '::HEADER_X_FORWARDED_ALL')
+        ? SymfonyRequest::HEADER_X_FORWARDED_ALL
+        : (SymfonyRequest::HEADER_X_FORWARDED_FOR
+            | SymfonyRequest::HEADER_X_FORWARDED_HOST
+            | SymfonyRequest::HEADER_X_FORWARDED_PORT
+            | SymfonyRequest::HEADER_X_FORWARDED_PROTO
+            | (defined(SymfonyRequest::class . '::HEADER_X_FORWARDED_PREFIX') ? SymfonyRequest::HEADER_X_FORWARDED_PREFIX : 0));
     if ($trustedProxies === '*') {
-        Request::setTrustedProxies(['0.0.0.0/0', '::/0'], Request::HEADER_X_FORWARDED_ALL);
+        SymfonyRequest::setTrustedProxies(['0.0.0.0/0', '::/0'], $headers);
     } else {
         $proxies = array_values(array_filter(array_map('trim', explode(',', $trustedProxies))));
         if ($proxies) {
-            Request::setTrustedProxies($proxies, Request::HEADER_X_FORWARDED_ALL);
+            SymfonyRequest::setTrustedProxies($proxies, $headers);
         }
     }
 }
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/core/bootstrap/app.php')
-    ->handleRequest(Request::capture());
+    ->handleRequest(IlluminateRequest::capture());
